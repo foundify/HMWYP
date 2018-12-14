c2a63739524392140950173b3cc6138b96c22a63
foundify <peter@centennialaviationacademy.com>
2018-12-14 15:24:25 -0500

    Initial launch

---
+++ results.js
@@ -0,0 +1,59 @@
+        function myFunction1() {
+          var x = document.getElementById("submission1");
+          var y = document.getElementById("results1");
+          if (x.style.display === "none") {
+            x.style.display = "none";
+            y.style.display = "block";
+          } else {
+            x.style.display = "none";
+            y.style.display = "block";
+          }
+        }
+
+        function myFunction2() {
+          var x = document.getElementById("submission2");
+          var y = document.getElementById("results2");
+          if (x.style.display === "none") {
+            x.style.display = "none";
+            y.style.display = "block";
+          } else {
+            x.style.display = "none";
+            y.style.display = "block";
+          }
+        }
+
+        function myFunction3() {
+          var x = document.getElementById("submission3");
+          var y = document.getElementById("results3");
+          if (x.style.display === "none") {
+            x.style.display = "none";
+            y.style.display = "block";
+          } else {
+            x.style.display = "none";
+            y.style.display = "block";
+          }
+        }
+
+        function myFunction4() {
+          var x = document.getElementById("submission4");
+          var y = document.getElementById("results4");
+          if (x.style.display === "none") {
+            x.style.display = "none";
+            y.style.display = "block";
+          } else {
+            x.style.display = "none";
+            y.style.display = "block";
+          }
+        }
+
+        function myFunction5() {
+          var x = document.getElementById("submission5");
+          var y = document.getElementById("results5");
+          if (x.style.display === "none") {
+            x.style.display = "none";
+            y.style.display = "block";
+          } else {
+            x.style.display = "none";
+            y.style.display = "block";
+          }
+        }
