5f874e012978f7dbca21262b3dd0de92264c231e
foundify <peter@centennialaviationacademy.com>
2018-12-15 13:18:24 -0500

    Smooth Transitions

--- results.js
+++ results.js
@@ -1,20 +1,20 @@
         function myFunction1() {
-            $('#submission1').hide(1000);
-            $('#results1').delay(1000).show(1000);
+            $('#submission1').fadeOut(500);
+            $('#results1').delay(500).fadeIn(500);
         }
         function myFunction2() {
-            $('#submission2').hide(1000);
-            $('#results2').delay(1000).show(1000);
+            $('#submission2').fadeOut(500);
+            $('#results2').delay(500).fadeIn(500);
         }
         function myFunction3() {
-            $('#submission3').hide(1000);
-            $('#results3').delay(1000).show(1000);
+            $('#submission3').fadeOut(500);
+            $('#results3').delay(500).fadeIn(500);
         }
         function myFunction4() {
-            $('#submission4').hide(1000);
-            $('#results4').delay(1000).show(1000);
+            $('#submission4').fadeOut(500);
+            $('#results4').delay(500).fadeIn(500);
         }
         function myFunction5() {
-            $('#submission5').hide(1000);
-            $('#results5').delay(1000).show(1000);
+            $('#submission5').fadeOut(500);
+            $('#results5').delay(500).fadeIn(500);
         }
